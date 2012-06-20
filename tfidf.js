04338e992cc5253c12ded63ab36d5489e9328a75
asus centos-6 localhost <joyce.chan@zoho.com>
2012-06-20 15:01:56 -0400

    all tests for tfidf library are completed

--- tfidf.js
+++ tfidf.js
@@ -6,24 +6,23 @@ var wordCount = function(document) {
 var freq = function(word,document){
   lst = document.split(" ");
   return _.filter(lst, function(wd){ return wd == word; }).length;
-}
+};
+
 var numDocsContaining = function(word,documentList){
   return _.filter(documentList, function(doc){ return freq(word,doc) > 0; }).length;
-}
+};
 
 var tf = function(word,document){
   return freq(word,document)/parseFloat(wordCount(document));
-} 
-/*
-def idf(word, documentList):
-  word_in_doc = float(numDocsContaining(word,documentList))
-  if word_in_doc !=0
-    return math.log(len(documentList) / word_in_doc)
-  return 0
-*/
+} ;
+
 var idf = function(word,documentList){
   word_in_doc = parseFloat(numDocsContaining(word,documentList));
   if (word_in_doc > 0){
     return Math.log(documentList.length/word_in_doc);
   }return 0;
-} 
+} ;
+
+var tfidf = function(word,document,documentList){
+  return (tf(word, document) * idf(word, documentList));
+};
