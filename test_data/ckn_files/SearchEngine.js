97ade78585dea8259de7a555f87951ed263a1039
asus centos-6 localhost <joyce.chan@zoho.com>
2012-06-21 14:34:37 -0400

    removed epicurious test files, just use index.html to test

--- test_data/ckn_files/SearchEngine.js
+++
@@ -1,7 +0,0 @@
-
-function SearchEngine() { }
-SearchEngine._path = '/dwr';
-
-SearchEngine.doSearch = function(p0, p1, callback) {
-    DWREngine._execute(SearchEngine._path, 'SearchEngine', 'doSearch', p0, p1, false, callback);
-}
